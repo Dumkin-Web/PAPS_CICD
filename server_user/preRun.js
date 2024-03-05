63ff796a8e9894028b30f6d0a376713ba626a4bd
Nikita Dumkin <ndumkin@mail.ru>
2024-03-05 23:42:46 +0500

    Pre run доделан

--- server_user/preRun.js
+++ server_user/preRun.js
@@ -1,7 +1,16 @@
 require('dotenv').config()
 const sequelize = require('./model/db')
+const {DataTypes} = require("sequelize");
+const {User} = require("./model");
 
-Promise.all([
-    sequelize.authenticate(),
-    sequelize.sync(),
-])
+const start = async () => {
+    try{
+        await User.sync()
+        await sequelize.authenticate()
+        await sequelize.sync()
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+start();
